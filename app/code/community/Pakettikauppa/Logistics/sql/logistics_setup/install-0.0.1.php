f915338979139aa16d94ee99242e1c26f9b396d7
Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>
2017-05-17 16:09:13 +0200

    additional database fields added
    Signed-off-by: Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>

--- app/code/community/Pakettikauppa/Logistics/sql/logistics_setup/install-0.0.1.php
+++ app/code/community/Pakettikauppa/Logistics/sql/logistics_setup/install-0.0.1.php
@@ -3,6 +3,14 @@ $installer = $this;
 $installer->startSetup();
 $order = $installer->getTable('sales_flat_order');
 $installer->getConnection()
+    ->addColumn($order, 'pickup_point_provider', array(
+            'nullable' => true,
+            'length' => 255,
+            'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'comment' => 'Pickup Point Provider'
+        )
+    );
+$installer->getConnection()
     ->addColumn($order, 'pickup_point_location', array(
             'nullable' => true,
             'length' => 255,
@@ -18,7 +26,71 @@ $installer->getConnection()
             'comment' => 'Pickup Point Zip'
         )
     );
+$installer->getConnection()
+  ->addColumn($order, 'pickup_point_id', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point ID'
+      )
+  );
+$installer->getConnection()
+  ->addColumn($order, 'pickup_point_name', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point Name'
+      )
+  );
+$installer->getConnection()
+  ->addColumn($order, 'pickup_point_street_address', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point Street Address'
+      )
+  );
+$installer->getConnection()
+  ->addColumn($order, 'pickup_point_postcode', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point Postcode'
+      )
+  );
+$installer->getConnection()
+  ->addColumn($order, 'pickup_point_city', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point City'
+      )
+  );
+$installer->getConnection()
+  ->addColumn($order, 'pickup_point_country', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point Country'
+      )
+  );
+$installer->getConnection()
+    ->addColumn($order, 'pickup_point_description', array(
+            'nullable' => true,
+            'length' => 255,
+            'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'comment' => 'Pickup Point Description'
+        )
+    );
 $quote = $installer->getTable('sales_flat_quote');
+$installer->getConnection()
+  ->addColumn($quote, 'pickup_point_provider', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point Provider'
+      )
+  );
 $installer->getConnection()
   ->addColumn($quote, 'pickup_point_location', array(
           'nullable' => true,
@@ -35,4 +107,60 @@ $installer->getConnection()
                 'comment' => 'Pickup Point Zip'
             )
         );
+$installer->getConnection()
+  ->addColumn($quote, 'pickup_point_id', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point ID'
+      )
+  );
+$installer->getConnection()
+  ->addColumn($quote, 'pickup_point_name', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point Name'
+      )
+  );
+$installer->getConnection()
+  ->addColumn($quote, 'pickup_point_street_address', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point Street Address'
+      )
+  );
+$installer->getConnection()
+  ->addColumn($quote, 'pickup_point_postcode', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point Postcode'
+      )
+  );
+$installer->getConnection()
+  ->addColumn($quote, 'pickup_point_city', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point City'
+      )
+  );
+$installer->getConnection()
+  ->addColumn($quote, 'pickup_point_country', array(
+          'nullable' => true,
+          'length' => 255,
+          'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+          'comment' => 'Pickup Point Country'
+      )
+  );
+$installer->getConnection()
+    ->addColumn($quote, 'pickup_point_description', array(
+            'nullable' => true,
+            'length' => 255,
+            'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'comment' => 'Pickup Point Description'
+        )
+    );
 $installer->endSetup();

c70f78909b982686d06e016da84a0fe9be95ae38
Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>
2017-06-15 13:26:34 +0200

    installer quote field added fix
    Signed-off-by: Filip Kunjadić - Ćulibrk <filip.kunjadic@pixel2go.com>

--- app/code/community/Pakettikauppa/Logistics/sql/logistics_setup/install-0.0.1.php
+++ app/code/community/Pakettikauppa/Logistics/sql/logistics_setup/install-0.0.1.php
@@ -98,4 +98,13 @@ $installer->getConnection()
             'comment' => 'Shipping Method Code'
         )
     );
+$quote = $installer->getTable('sales_flat_quote');
+$installer->getConnection()
+    ->addColumn($quote, 'pickup_point_zip', array(
+            'nullable' => true,
+            'length' => 255,
+            'type' => Varien_Db_Ddl_Table::TYPE_TEXT,
+            'comment' => 'Pickup Point Zip'
+        )
+    );
 $installer->endSetup();
